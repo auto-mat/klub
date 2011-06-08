eaf5e886ab92a1998f137a390b8ce933bc2064b7
Hynek Hanke <hanke@brailcom.org>
2011-06-09 00:14:30 +0200

    Regular contributors registration form finalization
    1) Some fields are marked as required in the
    registration form even if they are not required
    in the database model itself (to avoid conflicts
    with already existing data)
    
    2) Variable symbol is now auto-generated
    
    3) Regular payments field is set to True

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 # Create your views here.
+import datetime
 from django import forms, http
 from django.db import models
 from django.shortcuts import render_to_response
@@ -20,18 +21,42 @@ class RegularUserForm(forms.ModelForm):
 			  'regular_frequency', 'regular_amount', 'wished_tax_confirmation',
 			  'wished_welcome_letter', 'wished_information', 'public',
 		)
+		required = ('firstname', 'surname', 'street', 'city', 'country', 'zip_code',
+			    'language', 'email', 'telephone', 'regular_frequency', 'regular_amount')
 		widgets = {
 			'language': forms.RadioSelect, # should be set automatically
 		}
 
-	
+        def __init__(self, *args, **kwargs):
+		super(RegularUserForm, self).__init__(*args, **kwargs)
+		for field in self.Meta.required:
+			f = self.fields[field].required = True
+
 def regular(request):
     if request.method == 'POST': # If the form has been submitted...
         form = RegularUserForm(request.POST) # A form bound to the POST data
         if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-	    form.save()
+            # Check number of registrations so far today
+            # TODO: Lock DB access here (to ensure uniqueness of VS)
+            now = datetime.datetime.now()
+            reg_n_today = len(User.objects.filter(
+			    registered_support__gt = (
+				    now-datetime.timedelta(days=1))))
+	    for i in range(reg_n_today+1, 499):
+                variable_symbol = '%s%02d%02d%03d' % (
+			str(now.year)[-2:], now.month, now.day, 500+i)
+		if len(User.objects.filter(variable_symbol=variable_symbol)) == 0:
+                   break
+	    else:
+		assert 0, "Out of free variable symbols, date %s, reg_n_today=%d" % (now, reg_n_today)
+	    # variable_symbol is now unique in database
+	    # Create new user instance and fill in additional data
+            new_user = form.save(commit=False)
+	    new_user.regular_payments = True
+            new_user.variable_symbol = variable_symbol
+	    # Save new user instance
+	    new_user.save()
+	    # TODO: Unlock DB access here
             return http.HttpResponseRedirect('/thanks/') # Redirect after POST
     else:
         form = RegularUserForm() # An unbound form
