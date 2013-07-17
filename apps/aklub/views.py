50f8b55dc5ffbc480e4ebd0075fa654db80de00e
Petr Dlouhý <petr.dlouhy@email.cz>
2013-07-17 16:24:28 +0200

    add preambule to views.py

--- apps/aklub/views.py
+++ apps/aklub/views.py
@@ -1,8 +1,26 @@
+# -*- coding: utf-8 -*-
+# Author: Petr Dlouhý <petr.dlouhy@email.cz>
+#
+# Copyright (C) 2013 o.s. Auto*Mat
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 # Create your views here.
 import datetime, re
 from django import forms, http
 from django.db import models
-from django.db.models import Sum
 from django.shortcuts import render_to_response
 from django.utils.translation import ugettext as _
 from django.contrib.admin import widgets
