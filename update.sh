62f1e3dc1760de4eb94b211fb0fd998712486da7
Petr Dlouhý <petr.dlouhy@email.cz>
2013-11-05 18:01:34 +0100

    improve update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
 git pull
-env/bin/pip install -r requirements
+source env/bin/activate
+pip install -r requirements
 if [ "$1" = "migrate" ]; then
    echo "Backuping db..."
    mkdir db_backup
-   env/bin/python manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-aklub.json
+   ./manage.py dumpdata > db_backup/`date +"%y%m%d-%H:%M:%S"`-aklub.json
    echo "Migrating..."
-   env/bin/python manage.py migrate
+   ./manage.py migrate
 fi
 (cd apps/aklub/ && django-admin.py compilemessages)
-env/bin/python manage.py collectstatic --noinput
+./manage.py collectstatic --noinput
 touch wsgi.py
