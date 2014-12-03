a99a04086bfea4423213bedd59fc5d40607910e3
Petr Dlouhý <petr.dlouhy@email.cz>
2014-12-03 16:44:34 +0100

    fix update script

--- update.sh
+++ update.sh
@@ -18,7 +18,7 @@ env/bin/python env/bin/pip install -r requirements --upgrade
 if [ "$1" = "migrate" ]; then
    echo "Backuping db..."
    mkdir -p db_backup
-   sudo -u postgres pg_dump $db_name > db_backup/`date +"%y%m%d-%H:%M:%S"`-pnk.sql
+   sudo -u postgres pg_dump $db_name > db_backup/`date +"%y%m%d-%H:%M:%S"`.sql
    echo "Migrating..."
    env/bin/python ./manage.py migrate
 fi
