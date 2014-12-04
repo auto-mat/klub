a44c15c0b4983c8da28cc6e561cce7324367a792
Petr Dlouhý <petr.dlouhy@email.cz>
2014-12-04 12:29:34 +0100

    fix export to CSV error

--- apps/aklub/admin.py
+++ apps/aklub/admin.py
@@ -57,7 +57,7 @@ def export_as_csv_action(description="Export selected objects as CSV file",
             if exclude:
                 excludeset = set(exclude)
                 field_names = field_names - excludeset
-        response = HttpResponse(mimetype='text/csv')
+        response = HttpResponse(content_type='text/csv')
         response['Content-Disposition'] = 'attachment; filename=%s.csv' % unicode(opts).replace('.', '_')
         writer = csv.writer(response)
         if header:
