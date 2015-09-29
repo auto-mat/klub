a7854bc57aba360db8e2cd0f56388633f5007bbb
Petr Dlouhý <petr.dlouhy@email.cz>
2015-09-29 12:10:07 +0200

    merge migrations

---
+++ apps/aklub/migrations/0012_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aklub', '0008_auto_20150902_1242'),
+        ('aklub', '0011_auto_20150803_1731'),
+    ]
+
+    operations = [
+    ]
