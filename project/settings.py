04faa48550ee667f3d80c199058d9535f38ca345
Petr Dlouhý <petr.dlouhy@email.cz>
2015-11-05 13:58:33 +0100

    fixes afetr update

--- project/settings.py
+++ project/settings.py
@@ -79,6 +79,7 @@ TEMPLATE_CONTEXT_PROCESSORS += (
      'django.core.context_processors.media',
      'django.contrib.messages.context_processors.messages',
 )
+TEMPLATE_LOADERS = ("admin_tools.template_loaders.Loader", )
 
 MIDDLEWARE_CLASSES = (
 #    'johnny.middleware.LocalStoreClearMiddleware',  # disabled for django 1.4
