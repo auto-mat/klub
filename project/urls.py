96d5ff5f5bb83cf9cf42dcb31019b0d23d302946
Petr Dlouhý <petr.dlouhy@email.cz>
2016-02-09 15:27:58 +0100

    Django 1.10 compatibility fixes

--- project/urls.py
+++ project/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import include, url
+from aklub.views import stat_payments, stat_members
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/aklub/stat-members/', 'aklub.views.stat_members', name="stay-members"),
-    url(r'^admin/aklub/stat-payments/', 'aklub.views.stat_payments', name="stay-payments"),
+    url(r'^admin/aklub/stat-members/', stat_members, name="stay-members"),
+    url(r'^admin/aklub/stat-payments/', stat_payments, name="stay-payments"),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(admin.site.urls)),
     url(r'^admin/', include("massadmin.urls")),
