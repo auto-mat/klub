1756ee712340311f05ce284bb8594e7bffd01b61
Petr Dlouhý <petr.dlouhy@email.cz>
2016-08-11 14:59:23 +0200

    don't import darujme.cz in campaigns with "" api secret

--- apps/aklub/management/commands/darujme.py
+++ apps/aklub/management/commands/darujme.py
@@ -9,7 +9,7 @@ class Command(BaseCommand):
     help = 'Checks Darujme.cz for new payments'
 
     def handle(self, *args, **options):
-        for campaign in Campaign.objects.filter(darujme_api_secret__isnull=False):
+        for campaign in Campaign.objects.filter(darujme_api_secret__isnull=False).exclude(darujme_api_secret=""):
             print(campaign)
             payment, skipped = darujme.create_statement_from_API(campaign)
             print(payment)
