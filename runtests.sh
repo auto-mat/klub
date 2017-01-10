8046400ee533c2b5f4ba5c79da18033818f7a935
Petr Dlouhý <petr.dlouhy@email.cz>
2017-01-10 19:08:10 +0100

    fix Travis configuration

--- runtests.sh
+++ runtests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 if [ -z "$@" ]; then
-   env/bin/coverage run manage.py test aklub
+   coverage run manage.py test aklub
 else
-   env/bin/coverage run manage.py test $@
+   coverage run manage.py test $@
 fi
-env/bin/coverage html
+coverage html
