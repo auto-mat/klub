8d78a08fa355a78e465bcf6f48eec20c45b53363
Timothy Hobbs <timothy@hobbs.cz>
2018-12-19 16:12:34 +0100

    Configure S3

--- project/settings/heroku.py
+++ project/settings/heroku.py
@@ -17,6 +17,27 @@ ALLOWED_HOSTS = [
     "devel-klub.auto-mat.cz",
 ]
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_S3_HOST = os.environ.get('AWS_S3_HOST', 's3-eu-west-1.amazonaws.com')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME', 'klub')
+AWS_QUERYSTRING_AUTH = os.environ.get('AWS_QUERYSTRING_AUTH', False)
+AWS_QUERYSTRING_EXPIRE = os.environ.get('AWS_QUERYSTRING_EXPIRE', 60 * 60 * 24 * 365 * 10)
+
+if AWS_ACCESS_KEY_ID:
+    THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    CELERY_EMAIL_BACKEND = os.environ.get('CELERY_EMAIL_BACKEND', 'django_ses.SESBackend')
+    AWS_SES_REGION_NAME = 'eu-west-1'
+    AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
+
+    DBBACKUP_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    DBBACKUP_STORAGE_OPTIONS = {
+        'access_key': AWS_ACCESS_KEY_ID,
+        'secret_key': AWS_SECRET_ACCESS_KEY,
+        'bucket_name': 'klub-dbbackup',
+    }
+
 LOGGING['handlers']['logfile']['filename'] = "aklub.log" # noqa
 
 django_heroku.settings(locals())
