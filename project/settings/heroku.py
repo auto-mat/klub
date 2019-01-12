84a6c48a14bffcbe435e0d6a7fd31607710a626f
Timothy Hobbs <timothy@hobbs.cz>
2019-01-12 20:06:40 +0100

    Remove django-ses and it's configuration, we want to use sendgrid instead

--- project/settings/heroku.py
+++ project/settings/heroku.py
@@ -26,10 +26,6 @@ AWS_QUERYSTRING_EXPIRE = os.environ.get('AWS_QUERYSTRING_EXPIRE', 60 * 60 * 24 *
 if AWS_ACCESS_KEY_ID:
     THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    CELERY_EMAIL_BACKEND = os.environ.get('CELERY_EMAIL_BACKEND', 'django_ses.SESBackend')
-    AWS_SES_REGION_NAME = 'eu-west-1'
-    AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
-
     DBBACKUP_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DBBACKUP_STORAGE_OPTIONS = {
         'access_key': AWS_ACCESS_KEY_ID,
