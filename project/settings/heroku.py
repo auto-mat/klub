ca6d8a1cad9f2350e1386db0b4c4284aaa2a278a
Timothy Hobbs <timothy@hobbs.cz>
2019-01-13 18:28:45 +0100

    Revert "Remove django-ses and it's configuration, we want to use sendgrid instead"
    This reverts commit 84a6c48a14bffcbe435e0d6a7fd31607710a626f.

--- project/settings/heroku.py
+++ project/settings/heroku.py
@@ -4,13 +4,6 @@ import django_heroku
 
 from .base import *  # noqa
 
-if 'SENDGRID_USERNAME' in os.environ:
-    EMAIL_HOST_USER = os.environ['SENDGRID_USERNAME']
-    EMAIL_HOST = 'smtp.sendgrid.net'
-    EMAIL_PORT = 587
-    EMAIL_USE_TLS = True
-    EMAIL_HOST_PASSWORD = os.environ['SENDGRID_PASSWORD']
-
 ALLOWED_HOSTS = [
     "klub.auto-mat.cz",
     os.environ.get('HEROKU_APP_URL')
@@ -26,6 +19,10 @@ AWS_QUERYSTRING_EXPIRE = os.environ.get('AWS_QUERYSTRING_EXPIRE', 60 * 60 * 24 *
 if AWS_ACCESS_KEY_ID:
     THUMBNAIL_DEFAULT_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    CELERY_EMAIL_BACKEND = os.environ.get('CELERY_EMAIL_BACKEND', 'django_ses.SESBackend')
+    AWS_SES_REGION_NAME = 'eu-west-1'
+    AWS_SES_REGION_ENDPOINT = 'email.eu-west-1.amazonaws.com'
+
     DBBACKUP_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DBBACKUP_STORAGE_OPTIONS = {
         'access_key': AWS_ACCESS_KEY_ID,
