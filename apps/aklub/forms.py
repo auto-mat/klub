30403eac205772ea615dcded97135b8ffa0ef903
auto-mat <peta.novakova.pn@gmail.com>
2019-01-14 16:26:49 +0100

    update pipfile and date_joined2

--- apps/aklub/forms.py
+++ apps/aklub/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 import uuid
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 def username_validation(user, fields):
     if user.username == '':
