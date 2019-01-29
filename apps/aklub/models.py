1c6790e652ea67a070e7e53926c26b176b7040d7
Timothy Hobbs <timothy@hobbs.cz>
2019-01-29 17:11:05 +0100

    Link to old tax-confirmation file if no pdfsandwich exists

--- apps/aklub/models.py
+++ apps/aklub/models.py
@@ -2195,8 +2195,11 @@ class TaxConfirmation(models.Model):
         file = models.FileField(storage=OverwriteStorage())  # DEPRICATED!
 
         def get_pdf(self):
-            url = self.taxconfirmationpdf_set.get().pdf.url
-            return format_html("<a href='{}'>{}</a>", url, url)
+            try:
+                url = self.taxconfirmationpdf_set.get().pdf.url
+            except TaxConfirmationPdf.DoesNotExist:
+                url = self.file.url
+            return format_html("<a href='{}'>{}</a>", url, _('PDF file'))
 
         get_pdf.short_description = _("PDF")
 
