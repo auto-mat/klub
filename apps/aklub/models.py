308da637290a550f1cbf7fa4201ac4a56849be3e
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-08-09 23:23:57 +0200

    modify 'get_addresment' method according to the new 'UserProfile'/'CompanyProfile' db model

--- apps/aklub/models.py
+++ apps/aklub/models.py
@@ -690,12 +690,15 @@ class Profile(PolymorphicModel, AbstractUser):
         if self.first_name:
             return vokativ(self.first_name.strip()).title()
         if self.language == 'cs':
-            if self.sex == 'male':
-                return 'příteli Auto*Matu'
-            elif self.sex == 'female':
-                return 'přítelkyně Auto*Matu'
+            if hasattr(self, 'sex'):
+                if self.sex == 'male':
+                    return 'příteli Auto*Matu'
+                elif self.sex == 'female':
+                    return 'přítelkyně Auto*Matu'
+                else:
+                    return 'příteli/kyně Auto*Matu'
             else:
-                return 'příteli/kyně Auto*Matu'
+                return 'company'
         else:
             return 'Auto*Mat friend'
 
