d4dc73053cf9f339091e209a415aacf7b46f6a3d
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-08-26 19:48:55 +0200

    update new 'Profile' model dependencies and modify admin models related with new 'Profile', 'User/CompanyProfile' models

--- apps/aklub/models.py
+++ apps/aklub/models.py
@@ -853,7 +853,7 @@ class UserProfile(Profile):
     )
 
 
-@receiver(signals.m2m_changed, sender=UserProfile.administrative_units.through)
+@receiver(signals.m2m_changed, sender=Profile.administrative_units.through)
 def Userprofile_administrative_unit_changed(sender, **kwargs):
     user = kwargs['instance']
     for unit in user.administrative_units.all():
@@ -865,7 +865,7 @@ def Userprofile_administrative_unit_changed(sender, **kwargs):
 
 class Preference(models.Model):
     user = models.ForeignKey(
-        UserProfile,
+        Profile,
         blank=True,
         null=True,
         on_delete=models.CASCADE,
@@ -945,7 +945,7 @@ class Telephone(models.Model):
         blank=True,
     )
     user = models.ForeignKey(
-        UserProfile,
+        Profile,
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
@@ -1030,7 +1030,7 @@ class UserInCampaign(models.Model):
 
     # -- Basic personal information
     userprofile = models.ForeignKey(
-        UserProfile,
+        Profile,
         blank=False,
         default=None,
         on_delete=models.CASCADE,
@@ -1158,7 +1158,7 @@ class UserInCampaign(models.Model):
         default=False,
     )
     verified_by = models.ForeignKey(
-        UserProfile,
+        Profile,
         verbose_name=_("Verified by"),
         related_name='verified_users',
         null=True,
@@ -1235,8 +1235,8 @@ class UserInCampaign(models.Model):
     def person_name(self):
         try:
             return self.userprofile.__str__()
-        except UserProfile.DoesNotExist:  # This happens, when UserInCampaign is cached, but it is deleted already
-            return "No UserProfile"
+        except Profile.DoesNotExist:  # This happens, when UserInCampaign is cached, but it is deleted already
+            return "No Profile"
 
     person_name.short_description = _("Full name")
 
@@ -1566,7 +1566,7 @@ class DonorPaymentChannel(models.Model):
         null=True,
     )
     user = models.ForeignKey(
-        'aklub.UserProfile',
+        Profile,
         verbose_name=_("User"),
         on_delete=models.CASCADE,
         related_name="userchannels",
@@ -1911,8 +1911,8 @@ class DonorPaymentChannel(models.Model):
     def person_name(self):
         try:
             return self.user.__str__()
-        except UserProfile.DoesNotExist:  # This happens, when UserInCampaign is cached, but it is deleted already
-            return "No UserProfile"
+        except Profile.DoesNotExist:  # This happens, when UserInCampaign is cached, but it is deleted already
+            return "No Profile"
 
     person_name.short_description = _("Full name")
 
@@ -2184,7 +2184,7 @@ class Payment(models.Model):
 
 class BaseInteraction(models.Model):
     user = models.ForeignKey(
-        UserProfile,
+        Profile,
         verbose_name=_("User"),
         on_delete=models.CASCADE,
         # related_name="communications",
@@ -2276,7 +2276,7 @@ class Interaction(BaseInteraction):
         blank=True,
     )
     created_by = models.ForeignKey(
-        UserProfile,
+        Profile,
         verbose_name=_("Created by"),
         related_name='created_by_communication',
         null=True,
@@ -2284,7 +2284,7 @@ class Interaction(BaseInteraction):
         on_delete=models.SET_NULL,
     )
     handled_by = models.ForeignKey(
-        UserProfile,
+        Profile,
         verbose_name=_("Last handled by"),
         related_name='handled_by_communication',
         null=True,
@@ -2407,7 +2407,7 @@ class ConditionValues(object):
         for name in model_names:
             model = {
                 'User': User,
-                'UserProfile': UserProfile,
+                'Profile': Profile,
                 'UserInCampaign': UserInCampaign,
                 'Payment': Payment,
                 'User.source': Source,
@@ -2581,7 +2581,7 @@ class TerminalCondition(models.Model):
 
     variable = models.CharField(
         verbose_name=_("Variable"),
-        choices=ConditionValues(('User', 'UserProfile', 'UserInCampaign', 'User.source', 'User.last_payment')),
+        choices=ConditionValues(('User', 'Profile', 'UserInCampaign', 'User.source', 'User.last_payment')),
         help_text=_("Value or variable on left-hand side"),
         max_length=50,
         blank=True,
@@ -2855,7 +2855,7 @@ class MassCommunication(models.Model):
         default=False,
     )
     send_to_users = models.ManyToManyField(
-        UserProfile,
+        Profile,
         verbose_name=_("send to users"),
         help_text=_(
             "All users who should receive the communication"),
@@ -2919,7 +2919,7 @@ def confirmation_upload_to(instance, filename):
 
 class TaxConfirmation(models.Model):
     user_profile = models.ForeignKey(
-        UserProfile,
+        Profile,
         on_delete=models.CASCADE,
         null=True,
     )
