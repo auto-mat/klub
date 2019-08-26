adb40fb9b776017fa5e9aefca1800ab01b9adfad
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-08-26 20:41:04 +0200

    fix createsuperuser2 command use the child 'User/CompanyProfile' db model for the create superuser

--- apps/aklub/management/commands/createsuperuser2.py
+++ apps/aklub/management/commands/createsuperuser2.py
@@ -1,5 +1,9 @@
 # https://github.com/adamcharnock/swiftwind-heroku/blob/master/swiftwind_heroku/management/commands/create_superuser_with_password.py
+
+from aklub.models import CompanyProfile, UserProfile
+
 from django.contrib.auth.management.commands import createsuperuser
+from django.contrib.contenttypes.models import ContentType
 from django.core.management import CommandError
 
 
@@ -12,18 +16,56 @@ class Command(createsuperuser.Command):
             '--password', dest='password', default=None,
             help='Specifies the password for the superuser.',
         )
+        parser.add_argument(
+            '--profile',
+            dest='polymorphic_ctype_id',
+            choices=['user', 'company'],
+            default='user',
+            help='Choose how user profile save as, user or company profile '
+            'type, default is user profile',
+        )
+        parser.add_argument(
+            '--crn',
+            dest='crn',
+            type=str,
+            help='If profile type is the company, you must set company '
+            'registration number (crn)',
+        )
+        # Remove default --polymorphic_ctype_id arg (REQUIRED_FIELDS var)
+        # and replace with --profile arg
+        ctype_opt = '--polymorphic_ctype_id'
+        for pos, opt in enumerate(parser.__dict__['_actions']):
+            if opt.option_strings[0] == ctype_opt:
+                parser._handle_conflict_resolve(None, [(ctype_opt, parser._actions[pos])])
 
     def handle(self, *args, **options):
         password = options.get('password')
         username = options.get('username')
         database = options.get('database')
+        email = options.get('email')
+        profile_type = options.get('polymorphic_ctype_id')
+        crn = options.get('crn')
+
+        if profile_type == 'user':
+            profile_type_id = ContentType.objects.get(model=UserProfile._meta.model_name).id
+        else:
+            profile_type_id = ContentType.objects.get(model=CompanyProfile._meta.model_name).id
+            if not crn:
+                raise CommandError("--crn is required if specifying --profile company")
+        options['polymorphic_ctype_id'] = profile_type_id
+
+        model = ContentType.objects.get(id=profile_type_id).model_class()
+        if email and email in model.objects.all().values_list('email', flat=True):
+            raise CommandError("use another email address")
 
         if password and not username:
             raise CommandError("--username is required if specifying --password")
 
         super(Command, self).handle(*args, **options)
 
+        user = model._default_manager.db_manager(database).get(username=username)
         if password:
-            user = self.UserModel._default_manager.db_manager(database).get(username=username)
             user.set_password(password)
-            user.save()
+        if crn:
+            user.crn = crn
+        user.save()
