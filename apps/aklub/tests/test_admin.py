6e5f1ab753dfa964e5f7de95d70c98322e33ce45
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-09-02 06:08:28 +0200

    fix flake8 error

--- apps/aklub/tests/test_admin.py
+++ apps/aklub/tests/test_admin.py
@@ -19,8 +19,8 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 import pathlib
-from datetime import datetime
 import re
+from datetime import datetime
 
 from django.contrib import admin as django_admin, auth
 from django.contrib.auth.models import Group
