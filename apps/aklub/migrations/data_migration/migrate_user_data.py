3bb2a9ce4a5846d66b60052b38fa9b98dc342f01
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-09-05 06:53:56 +0200

    refactor function

--- apps/aklub/migrations/data_migration/migrate_user_data.py
+++ apps/aklub/migrations/data_migration/migrate_user_data.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib.contenttypes.models import ContentType
 
 ONETOONE_USER_PROFILE_MODEL_DATA = []
 USER_PROFILE_MODEL_DATA = []
@@ -13,10 +12,11 @@ def get_user_model_data(apps, schema_editor):
     user_profile_model = apps.get_model('aklub', 'UserProfile')
     global USER_PROFILE_MODEL_DATA
     global SPECIFIC_USER_PROFILE_DATA
-    if ContentType.objects.count() > 0:
+    content_type_model = apps.get_model('contenttypes', 'contenttype')
+    if content_type_model.objects.count() > 0:
         with schema_editor.connection.cursor() as cursor:
             cursor.execute("SELECT * FROM aklub_userprofile")
-            content_type_id = ContentType.objects.get(model=user_profile_model._meta.model_name).id
+            content_type_id = content_type_model.objects.get(model=user_profile_model._meta.model_name).id
             rows = [list(row) + [content_type_id] for row in cursor.fetchall()]
             columns = [col[0] for col in cursor.description]
             columns.append('polymorphic_ctype_id')
--- apps/aklub/migrations/data_migration/migrate_user_email.py
+++ apps/aklub/migrations/data_migration/migrate_user_email.py
@@ -5,8 +5,8 @@ from django.contrib.auth import get_user_model
 
 def migrate_user_email(apps, schema_editor):
     profile_model = get_user_model()
-    for user in profile_model.objects.all():
-        with schema_editor.connection.cursor() as cursor:
+    with schema_editor.connection.cursor() as cursor:
+        for user in profile_model.objects.all().only('email', 'id'):
             cursor.execute(
                 "INSERT INTO aklub_profileemail (email, user_id, is_primary) VALUES (%s, %s, %s)",
                 [user.email, user.id, True]
