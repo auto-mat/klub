82bf573b07f49e0b6968f7ec4a30792b0c3deebe
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-09-12 21:50:56 +0200

    fix get 'administrative_units' field value, during import 'ProfileAdmin', 'User/CompanyProfileAdmin' admin model

--- apps/aklub/profile_model_resources_mixin.py
+++ apps/aklub/profile_model_resources_mixin.py
@@ -112,7 +112,7 @@ def import_obj(self, obj, data, dry_run):  # noqa
             event=event,
             defaults={'VS': VS},
         )
-        if data.get('bank_account'):
+        if data.get('bank_account') and data.get("administrative_units"):
             bank_account, _ = BankAccount.objects.get_or_create(bank_account_number=data['bank_account'])
             bank_account.administrative_unit = obj.administrative_units.get(id=data["administrative_units"])
             bank_account.save()
