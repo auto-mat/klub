2d11822147d932c7c57412a723c2120f465643ce
Petr <petrkudela94@seznam.cz>
2019-09-19 16:38:16 +0200

    Preferenceinlines hide add button in change form

--- apps/aklub/admin.py
+++ apps/aklub/admin.py
@@ -504,7 +504,7 @@ class ProfileMergeForm(merge.MergeForm):
 class PreferenceInline(nested_admin.NestedStackedInline):
     model = Preference
     extra = 0
-    max_num = 0
+    max_num = 1
     can_delete = False
     fieldsets = (
         (None, {
