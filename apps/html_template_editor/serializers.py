56930a2deb5908c482188427bc371c3ef6d51df7
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-10-14 11:40:07 +0200

    add 'html_template_editor' app files

---
+++ apps/html_template_editor/serializers.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+import json
+import time
+
+from .models import TemplateContent, Images
+
+from rest_framework import serializers
+
+
+class TemplateContentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TemplateContent
+        fields = ('id',
+                  'created',
+                  'uuid',
+                  'page',
+                  'regions',
+                  'images')
+
+    def to_representation(self, instance):
+        ret = super(TemplateContentSerializer, self).to_representation(instance)
+        ret['regions'] = json.loads(ret['regions'])
+        return ret
+
+    def validate(self, data):
+        return data
+
+
+class ImagesSerializer(serializers.ModelSerializer):
+
+    size = serializers.ListField(
+        read_only=True,
+        child=serializers.IntegerField()
+    )
+
+    class Meta:
+        model = Images
+        fields = ('id',
+                  'created',
+                  'image',
+                  'name',
+                  'size',
+                  'edited_width',
+                  'edited_crop',
+                  'edited_direction',
+                  )
+
+    def to_representation(self, instance):
+        ret = super(ImagesSerializer, self).to_representation(instance)
+        if ret['image']:
+            # Modify the image URL by adding an _ignore param
+            # This will force the browser to reload the image
+            ret['image'] = "%s?_ignore=%s" % (ret['image'], time.time())
+        return ret
