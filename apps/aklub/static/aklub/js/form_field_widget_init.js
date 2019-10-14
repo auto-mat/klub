eb50843e937e09347912a73fda40787d41ba61a6
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-10-14 09:26:48 +0200

    add js modules handle load html template form field widget initialization

---
+++ apps/aklub/static/aklub/js/form_field_widget_init.js
@@ -0,0 +1,38 @@
+/*
+ * masscommunication_init.js
+ *
+ * MassCommunication form fields (html template widget) initialization
+ */
+
+(function ($, HtmlTemplateFormFieldWidget) {
+  $(document).ready(function () {
+    console.log(window.reverse)
+    var formOpts = {
+      editDialogPageContainer: 'template_page',
+      popoverDelay: 3000
+    }
+    var htmlTemplateFieldOpts = {
+      templateNameFieldId: '#id_template_name',
+      templateTypeFieldId: '#id_template_type',
+      templateDivFieldId: '#id_template',
+      hiddenTemplateFieldId: '#id_hidden_template',
+      templateTextareaFieldId: '#id_template_textarea',
+      templateDivFormFieldContainer: '.field-template',
+      templateTextAreaFormFieldContainer: '.field-template_textarea',
+    }
+    Object.assign(htmlTemplateFieldOpts, formOpts)
+    var htmlTemplateFieldWidget = new HtmlTemplateFormFieldWidget(htmlTemplateFieldOpts)
+
+    var htmlTemplateEnFieldOpts = {
+      templateNameFieldId: '#id_template_en_name',
+      templateTypeFieldId: '#id_template_en_type',
+      templateDivFieldId: '#id_template_en',
+      hiddenTemplateFieldId: '#id_hidden_template_en',
+      templateTextareaFieldId: '#id_template_en_textarea',
+      templateDivFormFieldContainer: '.field-template_en',
+      templateTextAreaFormFieldContainer: '.field-template_en_textarea',
+    }
+    Object.assign(htmlTemplateEnFieldOpts, formOpts)
+    var htmlTemplateEnFieldWidget = new HtmlTemplateFormFieldWidget(htmlTemplateEnFieldOpts)
+  })
+}(jQuery, HtmlTemplateFormFieldWidget)
