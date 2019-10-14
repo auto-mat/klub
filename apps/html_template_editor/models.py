48e0e6ba4c84d4f8503e02c47755f6e3d301833f
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-10-14 11:40:07 +0200

    add 'html_template_editor' app files

---
+++ apps/html_template_editor/models.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+import uuid
+
+# from django.contrib.auth import get_user_model
+from django.db import models
+
+# User = get_user_model()
+
+
+class TemplateContent(models.Model):
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+    )
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+    )
+    images = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    page = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    regions = models.TextField(
+        blank=True,
+        default='',
+        null=True,
+    )
+
+
+class Images(models.Model):
+
+    created = models.DateTimeField(
+        auto_now_add=True,
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+    )
+    image = models.ImageField(
+        upload_to='images',
+        height_field=None,
+        width_field=None,
+        max_length=100,
+        null=True,
+    )
+    name = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+    )
+    edited_width = models.IntegerField(
+        blank=True,
+        null=True,
+    )
+    edited_crop = models.CharField(
+        max_length=80,
+        blank=True,
+        null=True,
+    )
+    edited_direction = models.CharField(
+        max_length=5,
+        blank=True,
+        null=True,
+    )
+
+    def size(self):
+        return [self.image.width, self.image.height]
+
+
+# class FileUpload(models.Model):
+#     created = models.DateTimeField(
+#         auto_now_add=True,
+#     )
+#     owner = models.ForeignKey(
+#         User,
+#         on_delete=models.SET_NULL,
+#         null=True,
+#     )
+#     datafile = models.FileField(
+#         upload_to='files',
+#     )
---
+++ apps/html_template_editor/views.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+
+from .models import TemplateContent
+from .serializers import TemplateContentSerializer
+
+from rest_framework import filters, generics
+from rest_framework.parsers import FormParser, MultiPartParser
+
+#
+# Mixin for all company views.
+# Defines serializers, queryset and permissions
+#
+
+
+class TemplateContentMixin(object):
+    def get_queryset(self):
+        return TemplateContent.objects.filter()
+
+    def get_serializer_class(self):
+        return TemplateContentSerializer
+
+
+class TemplateContentAdd(TemplateContentMixin, generics.CreateAPIView):
+    parser_classes = (MultiPartParser, FormParser,)
+
+    def perform_create(self, serializer):
+        regions = self.request.data.get('regions')
+        images = self.request.data.get('images')
+        page = self.request.data.get('page')
+
+        serializer.save(
+            regions=regions,
+            images=images,
+            page=page,
+        )
+
+
+class TemplateContentRetrieve(TemplateContentMixin, generics.RetrieveAPIView):
+    lookup_field = 'uuid'
+
+
+class TemplateContentUpdate(TemplateContentMixin, generics.UpdateAPIView):
+    def post(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def perform_update(self, serializer):
+        serializer.save()
+
+
+class TemplateContentList(TemplateContentMixin, generics.ListAPIView):
+    filter_backends = (filters.OrderingFilter,)
+    ordering_fields = ('created')
+    ordering = ('-created',)
+
+
+class TemplateContentDelete(TemplateContentMixin, generics.DestroyAPIView):
+    lookup_field = 'uuid'
