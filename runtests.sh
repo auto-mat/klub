1c6a6ab9a6c0c6464c44da5006c544b633bd942d
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-10-15 10:01:35 +0200

    test 'html_template_editor' app

--- runtests.sh
+++ runtests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 if [ -z "$@" ]; then
-   coverage run manage.py test aklub
-   coverage run manage.py test api
+    coverage run manage.py test aklub
+    coverage run manage.py test api
+    coverage run manage.py test html_template_editor
 else
-   coverage run manage.py test $@
+    coverage run manage.py test $@
 fi
 coverage html
