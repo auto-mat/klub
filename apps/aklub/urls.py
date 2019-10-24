1e184b2558d372c30e27c4ac129212cc6d0c520e
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-10-24 08:25:47 +0200

    fix flake8 error

--- apps/aklub/urls.py
+++ apps/aklub/urls.py
@@ -1,8 +1,8 @@
 from aklub import views
 
-from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from . import tasks # noqa
--- apps/aklub/utils.py
+++ apps/aklub/utils.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 import datetime
 import os
-from operator import itemgetter
 import pathlib
+from operator import itemgetter
 
 from django.contrib import messages
 from django.db import models
