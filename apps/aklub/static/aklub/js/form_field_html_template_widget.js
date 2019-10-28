11d2d548dcf200e24e02749f956d032052f905cb
Tomas Zigo <tomas.zigo@slovanet.sk>
2019-10-28 09:43:18 +0100

    wrap editable email template text html element/img into the table layout design convert video iframe object into the clickable video img thumbnail (youtube/vimeo) convert img css float position into the table layout design

--- apps/aklub/static/aklub/js/form_field_html_template_widget.js
+++ apps/aklub/static/aklub/js/form_field_html_template_widget.js
@@ -86,12 +86,120 @@ var HtmlTemplateFormFieldWidget = (function ($) {
         if (this.newEmptyTemplateName) {
           this.addEditTemplateConfirmEvent()
         }
+        // var htmlDocument = this.getIframeTemplateContent()
+        // this.convertVideoImgThumbnailToIframe(htmlDocument)
       },
-      replaceImgSrc: function (element) {
-        var imgs = element.find('img')
+      adjustListCssProperties: function (element) {
+        if (element.is('ul') || element.is('ol')) {
+          element.css({
+            margin: '10px',
+            padding: '15px'
+          })
+        }
+      },
+      convertTextCssPositionToTable: function (editContainer) {
+        var elementCssClass, tableWrapper, that
+        that = this
+        editContainer.find('p, ul, ol, h1, h2, h3, h4, h5, h6').each(function () {
+          elementCssClass = $(this).attr('class') ? $(this).attr('class').split(' ') : []
+          switch (elementCssClass) {
+            case elementCssClass.indexOf('text-left') > -1:
+              tableWrapper = that.setElementPosition('left', '', '')
+              break
+            case elementCssClass.indexOf('text-right') > -1:
+              tableWrapper = that.setElementPosition('right', '', '')
+              break
+            case elementCssClass.indexOf('text-center') > -1:
+              tableWrapper = that.setElementPosition('center', '', '')
+              break
+            default:
+              tableWrapper = that.setElementPosition('center', '', '')
+          }
+          that.adjustListCssProperties($(this))
+          tableWrapper.find('td').append($(this).clone())
+          $(this).after(tableWrapper)
+          $(this).remove()
+        })
+      },
+      setElementPosition: function (align, width, heigth) {
+        var table = $('<table></table>')
+        table.attr({
+          cellspacing: 0,
+          cellpadding: 0,
+          border: 0,
+          width: '100%'
+        })
+        var tableBody = $('<tbody></tbody>')
+        var tableRow = $('<tr></tr>')
+        var tableCell = $('<td></td>')
+        tableCell.css({
+          border: 'none',
+          padding: 0,
+          margin: 0
+        })
+        tableCell.attr({
+          align: align,
+          width: width,
+          heigth: heigth
+        })
+        return table.append(tableBody.append(tableRow.append(tableCell)))
+      },
+      convertImgFloatCssPositionToTable: function (img, editContainer, position) {
+        var padding = 15
+        var paddingValues = position === 'left' ? '0 0 0 ' + padding + 'px' : '0 ' + padding + 'px' + ' 0 0'
+        var tableCell = $('<td></td>')
+        tableCell.css({
+          border: 'none',
+          padding: paddingValues,
+          margin: '0'
+        })
+        var contextElement = img.closest('.video').length > 0 ? img.closest('.video').next().clone() : img.next().clone()
+        var contextWidth = parseFloat(editContainer.find('#content_table').width() - parseFloat(img.attr('width')) - padding)
+        contextElement.css('width', contextWidth)
+        tableCell.attr('width', contextWidth)
+        tableCell.append(contextElement)
+        img.closest('.video').length > 0 ? img.closest('.video').next().remove() : img.next().remove()
+        return tableCell
+      },
+      convertImageCssPostionToTable: function (img, editContainer) {
+        var tableWrapper, marginLeft, marginRight, floatPosition, tableCell
+        img = img.find('img').length > 0 ? img.find('img') : img
+        marginLeft = img.css('margin-left').split('.')[0]
+        marginRight = img.css('margin-right').split('.')[0]
+        floatPosition = img.css('float')
+        img.css('display', '')
+        if ((marginLeft === marginRight) && (floatPosition === 'none')) {
+          tableWrapper = this.setElementPosition('center', '', '')
+        } else if (marginLeft > marginRight) {
+          tableWrapper = this.setElementPosition(floatPosition, img.attr('width'), img.attr('height'))
+          tableCell = this.convertImgFloatCssPositionToTable(img, editContainer, floatPosition)
+        } else if (marginRight > marginLeft) {
+          tableWrapper = this.setElementPosition(floatPosition, img.attr('width'), img.attr('height'))
+          tableCell = this.convertImgFloatCssPositionToTable(img, editContainer, floatPosition)
+        } else if ((floatPosition === 'left') || (floatPosition === 'right')) {
+          tableWrapper = this.setElementPosition(floatPosition, img.attr('width'), img.attr('height'))
+          tableCell = this.convertImgFloatCssPositionToTable(img, editContainer, floatPosition)
+        }
+        tableWrapper.find('td').append(img.closest('.video').length > 0 ? img.closest('.video').clone() : img.clone())
+        floatPosition === 'left' ? tableWrapper.find('tr').append(tableCell) : tableWrapper.find('tr').prepend(tableCell)
+        img.closest('.video').length > 0 ? img.closest('.video').after(tableWrapper) : img.after(tableWrapper)
+        img.closest('.video').lnegth > 0 ? img.closest('.video').remove() : img.remove()
+      },
+      replaceImgSrc: function (editContainer) {
+        var that = this
+        var imgs = editContainer.find('img')
         imgs.each(function () {
-          var imgSrc = window.origin + $(this).attr('src')
-          $(this).attr('src', imgSrc)
+          var clickableVideoThumbnail = $(this).closest('.video')
+          var img = clickableVideoThumbnail.length > 0 ? clickableVideoThumbnail : $(this)
+          if (clickableVideoThumbnail.length === 0) {
+            var src = $(this).attr('src')
+            var regexPattern = new RegExp(window.origin)
+            if (!(src.match(regexPattern))) {
+              var imgSrc = window.origin + $(this).attr('src')
+              $(this).attr('src', imgSrc)
+            }
+          }
+          that.convertImageCssPostionToTable(img, editContainer)
         })
       },
       eraseNewTemplateName: function () {
@@ -118,19 +226,86 @@ var HtmlTemplateFormFieldWidget = (function ($) {
         confirmEditBtn.unbind('click')
         confirmEditBtn.bind('click', this.confirmTemplateEdit.bind(this))
       },
-      convertCssToInlineStyle: function () {
-        var templatePage, htmlDocument, article
+      convertVideoImgThumbnailToIframe: function (htmlDocument) {
+        $(htmlDocument).find('.video').each(function () {
+          var img = $(this).find('img')
+          var iFrame = $('<iframe></iframe>')
+          iFrame.attr({
+            width: img.attr('width'),
+            height: img.attr('height'),
+            src: img.attr('src')
+          })
+          $(this).after(iFrame)
+          $(this).remove()
+        })
+      },
+      convertVideoIframeToImgThumbnail: function (htmlDocument) {
+        var youtubeRegex, vimeoRegex, clickableVideoThumbnail, videoThumbnail, videoId, videoThumbnailSrc
+        youtubeRegex = new RegExp('youtube.com.*(v=|/embed/)(.{11})')
+        vimeoRegex = new RegExp('vimeo.com.*(.{8})')
+        $(htmlDocument).find('iframe').each(function () {
+          var iframeSrc = $(this).attr('src')
+          var iframeCssFloatProperty = $(this).css('float')
+          var iframeWidth = $(this).attr('width')
+          var iframeHeight = $(this).attr('height')
+          clickableVideoThumbnail = $('<a href="" class="video"></a>')
+          clickableVideoThumbnail.attr('href', iframeSrc)
+          videoThumbnail = $('<img src="">')
+          videoThumbnail.attr({
+            width: iframeWidth,
+            height: iframeHeight
+          })
+          if (iframeSrc.match(youtubeRegex)) {
+            videoId = iframeSrc.match(youtubeRegex).pop()
+            clickableVideoThumbnail.attr('id', videoId)
+            clickableVideoThumbnail.addClass('youtube')
+            videoThumbnailSrc = '//img.youtube.com/vi/' + videoId + '/0.jpg'
+            videoThumbnail.attr('src', videoThumbnailSrc)
+          } else if (iframeSrc.match(vimeoRegex)) {
+            videoId = iframeSrc.match(vimeoRegex).pop()
+            clickableVideoThumbnail.attr('id', videoId)
+            clickableVideoThumbnail.addClass('vimeo')
+            $.ajax({
+              type: 'GET',
+              url: 'http://vimeo.com/api/v2/video/' + videoId + '.json',
+              jsonp: 'callback',
+              dataType: 'jsonp',
+              context: { videoThumbnail: videoThumbnail },
+              success: function (data) {
+                videoThumbnailSrc = data[0].thumbnail_large
+                this.videoThumbnail.attr('src', videoThumbnailSrc)
+              }
+            })
+          }
+          if (videoThumbnail) {
+            videoThumbnail.css('float', iframeCssFloatProperty)
+            // Add video thumbnail
+            clickableVideoThumbnail.append(videoThumbnail)
+            $(this).after(clickableVideoThumbnail)
+            // Remove video iframe
+            $(this).remove()
+          }
+        })
+      },
+      convertCssToInlineStyle: function (htmlDocument) {
+        var editContainer = $(htmlDocument).find('article')
+        // Convert email template external css into inline
+        this.inlineStyler(editContainer)
+        this.replaceImgSrc(editContainer)
+        this.convertTextCssPositionToTable(editContainer)
+        this.$templateDivField.html(editContainer)
+        this.$hiddenTemplateField.val(editContainer.html())
+      },
+      getIframeTemplateContent: function () {
+        var templatePage, htmlDocument
         templatePage = document.querySelector('#' + this.editDialogPageContainer)
         htmlDocument = templatePage.contentDocument
-        article = $(htmlDocument).find('article')
-        // Convert email template external css into inline
-        this.inlineStyler(article)
-        // this.replaceImgSrc(article)
-        this.$templateDivField.html(article)
-        this.$hiddenTemplateField.val(article.html())
+        return htmlDocument
       },
       closeEditTemplateDialog: function (e) {
-        this.convertCssToInlineStyle()
+        var htmlDocument = this.getIframeTemplateContent()
+        this.convertVideoIframeToImgThumbnail(htmlDocument)
+        this.convertCssToInlineStyle(htmlDocument)
       },
       setLoadTemplateParam: function (templateName, urlName) {
         var url
