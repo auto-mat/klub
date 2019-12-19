e6f563920c29ee4c714c8152c87b676553ccaed3
PetrKudy <44304564+PetrKudy@users.noreply.github.com>
2019-12-19 15:32:54 +0100

    Connectors import (#180)
    * add administrative units connectors to import_export_celery
    
    * register connectors app
    
    * missing migration file
    
    * edit migration file

---
+++ apps/import_export_celery_edit/models.py
@@ -0,0 +1,33 @@
+from aklub.models import AdministrativeUnit
+
+from django.db import models
+
+from import_export_celery.models import ExportJob, ImportJob
+
+
+class ImportConnector(models.Model):
+    importjob = models.OneToOneField(
+                     ImportJob,
+                     on_delete=models.CASCADE,
+    )
+
+    administrative_unit = models.ForeignKey(
+        AdministrativeUnit,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+
+
+class ExportConnector(models.Model):
+    exportjob = models.OneToOneField(
+                     ExportJob,
+                     on_delete=models.CASCADE,
+    )
+
+    administrative_unit = models.ForeignKey(
+        AdministrativeUnit,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
