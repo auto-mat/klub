e0276ce34be63af47e0b801b4647fb2d7acb674c
petrkudela94@seznam.cz <petrkudela94@seznam.cz>
2020-01-08 10:06:57 +0100

    fix timeline test

--- apps/aklub/tests/test_timeline.py
+++ apps/aklub/tests/test_timeline.py
@@ -19,22 +19,24 @@ class TimelineTest(CreateSuperUserMixin, TestCase):
     @freeze_time("2017-5-1")
     def test_timeline(self):
         foo_user = user_profile_recipe.make(id=2978, first_name="Foo", email="foo@bar.cz")
-        foo_user_pc = donor_payment_channel_recipe.make(user=foo_user)
+        unit = mommy.make('aklub.AdministrativeUnit')
+        bank_acc = mommy.make('aklub.BankAccount', administrative_unit=unit)
+        foo_user_pc = donor_payment_channel_recipe.make(user=foo_user, money_account=bank_acc)
         mommy.make("aklub.Payment", amount=350, date="2016-01-02", user_donor_payment_channel=foo_user_pc, type="cash")
-        mommy.make("aklub.Interaction", user=foo_user, subject="interaction subject")
+        mommy.make("aklub.Interaction", user=foo_user, subject="interaction subject", administrative_unit=unit)
         # Search by email
         urlsafe_query = query_to_base64({
             'search_string': 'foo@bar.cz',
         })
         address = reverse('helpdesk:timeline_ticket_list', args=[urlsafe_query])
         response = self.client.get(address)
-        self.assertEqual(response.json()['events'][1]['text']['headline'], 'interaction subject')
-        self.assertEqual(response.json()['events'][2]['text']['headline'], '350 Kč')
+        self.assertEqual(response.json()['events'][1]['text']['headline'], '<p style="color:#000000;">interaction subject</p>')
+        self.assertEqual(response.json()['events'][2]['text']['headline'], '<p style="color:#000000;">350 Kč</p>')
         # Search by pk
         urlsafe_query = query_to_base64({
             'search_profile_pks': [foo_user.pk],
         })
         address = reverse('helpdesk:timeline_ticket_list', args=[urlsafe_query])
         response = self.client.get(address)
-        self.assertEqual(response.json()['events'][1]['text']['headline'], 'interaction subject')
-        self.assertEqual(response.json()['events'][2]['text']['headline'], '350 Kč')
+        self.assertEqual(response.json()['events'][1]['text']['headline'], '<p style="color:#000000;">interaction subject</p>')
+        self.assertEqual(response.json()['events'][2]['text']['headline'], '<p style="color:#000000;">350 Kč</p>')
