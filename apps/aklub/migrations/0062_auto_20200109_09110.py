1d0a8ac1f59d0d2d387d33f7492664552a603d3a
Petr Kůdela <petrkudela94@seznam.cz>
2020-03-04 14:13:46 +0100

    fixing old data migration mistakes ...

--- apps/aklub/migrations/0062_auto_20200109_09110.py
+++ apps/aklub/migrations/0062_auto_20200109_09110.py
@@ -2,8 +2,6 @@
 import django.core.validators
 from django.db import migrations, models
 
-from aklub.models import AdministrativeUnit, MoneyAccount, AccountStatements, Interaction, Preference, TaxConfirmation
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,32 +9,43 @@ class Migration(migrations.Migration):
     ]
 
     def default_values_money_acccount(apps, schema_editor):
-        for obj in MoneyAccount.objects.all():
+        db_alias = schema_editor.connection.alias
+        MoneyAccount = apps.get_model("aklub", "MoneyAccount")
+        AdministrativeUnit = apps.get_model("aklub", "AdministrativeUnit")
+        unit, _ = AdministrativeUnit.objects.using(db_alias).get_or_create(name='change_this_unit_then_delete')
+        for obj in MoneyAccount.objects.using(db_alias).all():
             if not hasattr(obj, 'administrative_unit'):
-                unit, _ = AdministrativeUnit.objects.get_or_create(name='change_this_unit_then_delete')
                 obj.administrative_unit = unit
                 obj.save()
 
     def default_values_preference(apps, schema_editor):
-        for obj in Preference.objects.all():
+        db_alias = schema_editor.connection.alias
+        Preference = apps.get_model("aklub", "Preference")
+        AdministrativeUnit = apps.get_model("aklub", "AdministrativeUnit")
+        unit, _ = AdministrativeUnit.objects.using(db_alias).get_or_create(name='change_this_unit_then_delete')
+
+        for obj in Preference.objects.using(db_alias).all():
             if not hasattr(obj, 'administrative_unit'):
-                unit, _ = AdministrativeUnit.objects.get_or_create(name='change_this_unit_then_delete')
                 obj.administrative_unit = unit
                 obj.save()
 
     def default_values_taxconfirmation(apps, schema_editor):
-        for obj in TaxConfirmation.objects.all():
+        db_alias = schema_editor.connection.alias
+        TaxConfirmation = apps.get_model("aklub", "TaxConfirmation")
+        AdministrativeUnit = apps.get_model("aklub", "AdministrativeUnit")
+        unit, _ = AdministrativeUnit.objects.using(db_alias).get_or_create(name='change_this_unit_then_delete')
+
+        for obj in TaxConfirmation.objects.using(db_alias).all():
             if not hasattr(obj, 'administrative_unit'):
-                unit, _ = AdministrativeUnit.objects.get_or_create(name='change_this_unit_then_delete')
                 obj.administrative_unit = unit
                 obj.save()
 
 
 
     operations = [
-        migrations.RunPython(default_values_money_acccount),
-        migrations.RunPython(default_values_preference),
-        migrations.RunPython(default_values_taxconfirmation),
+        migrations.RunPython(default_values_money_acccount, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(default_values_preference, reverse_code=migrations.RunPython.noop),
+        migrations.RunPython(default_values_taxconfirmation, reverse_code=migrations.RunPython.noop),
 
         migrations.AlterField(
             model_name='moneyaccount',
