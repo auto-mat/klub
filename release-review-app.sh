7690ba1a6be73cca6d70a0159cdae8c3147a9ed5
Petr Dlouhý <petr.dlouhy@email.cz>
2020-05-13 11:18:09 +0200

    add django celery beat configuration to review apps

--- release-review-app.sh
+++ release-review-app.sh
@@ -2,4 +2,5 @@
 sh ./release.sh
 python3 manage.py createsuperuser2 --username=admin --password=$REVIEW_APP_PASSWORD  --noinput --email 'blank@email.com'
 python3 manage.py loaddata apps/aklub/fixtures/conditions.json
+python3 manage.py loaddata apps/aklub/fixtures/django_celery_beat.json
 python3 manage.py dump_data
