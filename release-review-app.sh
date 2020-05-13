8113698b0baaf858aef3b6550dca54a493813402
Petr Dlouhý <petr.dlouhy@email.cz>
2020-05-13 10:17:21 +0200

    load conditions data to review app

--- release-review-app.sh
+++ release-review-app.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh ./release.sh
 python3 manage.py createsuperuser2 --username=admin --password=$REVIEW_APP_PASSWORD  --noinput --email 'blank@email.com'
+python3 manage.py loaddata apps/aklub/fixtures/conditions.json
