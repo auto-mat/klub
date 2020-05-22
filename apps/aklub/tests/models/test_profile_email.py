09d4655b43adb3e6eab1d50e7323753ca745be04
Petr Kůdela <petrkudela94@seznam.cz>
2020-05-22 12:59:28 +0200

    remove company from profileemail test and make own test

--- apps/aklub/tests/models/test_profile_email.py
+++ apps/aklub/tests/models/test_profile_email.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 
 from model_mommy import mommy
 
-from ...models import Profile, ProfileEmail
+from ...models import ProfileEmail
 
 
 class ProfileEmailTest(TestCase):
@@ -12,33 +12,29 @@ class ProfileEmailTest(TestCase):
 
     def test_create_profile_email(self):
         """ Test create ProfileEmail model """
-        # User/CompanyProfile model name
-        child_models = [model._meta.model_name for model in Profile.__subclasses__()]
-        for model in child_models:
-            username = 'test.{}'.format(model)
-            profile = mommy.make(
-                model,
-                username=username,
-            )
-            self.assertEqual(profile.email, None)
+        username = 'test_user'
+        profile = mommy.make(
+            'aklub.UserProfile',
+            username=username,
+        )
+        self.assertEqual(profile.email, None)
 
-            user_profile_email1 = '{0}1@{0}1.test'.format(username)
-            mommy.make(
-                'aklub.ProfileEmail',
-                email=user_profile_email1,
-                is_primary=True,
-                user=profile,
-            )
-            self.assertEqual(Profile.objects.get(username=username).email, user_profile_email1)
-            emails = ProfileEmail.objects.filter(user=profile)
-            self.assertEqual(emails.count(), 1)
+        user_profile_email1 = mommy.make(
+            'aklub.ProfileEmail',
+            email='test_email@bla.com',
+            is_primary=True,
+            user=profile,
+        )
+        self.assertEqual(profile.profileemail_set.first(), user_profile_email1)
+        emails = ProfileEmail.objects.filter(user=profile)
+        self.assertEqual(emails.count(), 1)
 
-            user_profile_email2 = '{0}2@t{0}2.test'.format(username)
-            mommy.make(
-                'aklub.ProfileEmail',
-                email=user_profile_email2,
-                is_primary='',
-                user=profile,
-            )
-            emails = ProfileEmail.objects.filter(user=profile)
-            self.assertEqual(emails.count(), 2)
+        mommy.make(
+            'aklub.ProfileEmail',
+            email='test_email@blah.com2',
+            is_primary='',
+            user=profile,
+        )
+
+        emails = ProfileEmail.objects.filter(user=profile)
+        self.assertEqual(emails.count(), 2)
