0d66625bcd72c769b31d251c9ee217725dad1f93
Petr Kůdela <petrkudela94@seznam.cz>
2020-05-27 10:07:53 +0200

    move get_dpch func to utils

--- apps/api/views.py
+++ apps/api/views.py
@@ -1,7 +1,4 @@
-
-import datetime
-
-from aklub.models import CompanyProfile, DonorPaymentChannel, ProfileEmail, Telephone,  UserProfile
+from aklub.models import CompanyProfile, ProfileEmail, Telephone,  UserProfile
 
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 
@@ -10,24 +7,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import GetDpchCompanyProfileSerializer, GetDpchUserProfileSerializer
-
-
-def get_or_create_dpch(serializer, profile):
-    dpch, created = DonorPaymentChannel.objects.get_or_create(
-                    event=serializer.validated_data['event'],
-                    money_account=serializer.validated_data['money_account'],
-                    user=profile,
-                    )
-    if created:
-        dpch.expected_date_of_first_payment = datetime.date.today() + datetime.timedelta(days=3)
-        if serializer.validated_data.get('regular'):
-            dpch.regular_payments = 'regular'
-        else:
-            dpch.regular_payments = 'onetime'
-        dpch.regular_amount = serializer.validated_data['amount']
-        dpch.generate_VS()
-        dpch.save()
-    return dpch.VS
+from .utils import get_or_create_dpch
 
 
 class CreateDpchUserProfileView(APIView):
