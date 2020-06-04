c4da5e0b9fc2a79953e147fc4f1b2d9392081fd8
Petr Kůdela <petrkudela94@seznam.cz>
2020-06-04 15:21:31 +0200

    missing slashs

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 
-    path('userprofile/vs', views.CreateDpchUserProfileView.as_view(), name='userprofile_vs'),
-    path('companyprofile/vs', views.CreateDpchCompanyProfileView.as_view(), name='companyprofile_vs'),
+    path('userprofile/vs/', views.CreateDpchUserProfileView.as_view(), name='userprofile_vs'),
+    path('companyprofile/vs/', views.CreateDpchCompanyProfileView.as_view(), name='companyprofile_vs'),
 
 ]
