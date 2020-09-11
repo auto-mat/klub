34cb0a3f5f87dd120a41817c3388b06e99c56ed8
Petr Kůdela <petrkudela94@seznam.cz>
2020-09-11 13:45:18 +0200

    tests => account parse csv reiffeisenbank

--- apps/aklub/tests/models/test_account_statements.py
+++ apps/aklub/tests/models/test_account_statements.py
@@ -246,6 +246,46 @@ class AccountStatementTests(RunCommitHooksMixin, TestCase):
 
         self.assertEqual(donor_channel.payment_set.get(date=datetime.date(2019, 8, 13)), a1.payment_set.get(account='9999999999'))
 
+    def test_bank_new_statement_raiffeisenbank(self):
+        recipient_account = mommy.make("aklub.bankaccount", bank_account_number='233223/12', administrative_unit=self.unit)
+        with open("apps/aklub/test_data/pohyby_raiffeisenbank.csv", "rb") as f:
+            a = AccountStatements(csv_file=File(f), type="account_raiffeisenbank", administrative_unit=self.unit)
+            a.clean()
+            a.save()
+        donor_channel = DonorPaymentChannel.objects.get(VS=120127010)
+
+        self.run_commit_hooks()
+        a1 = AccountStatements.objects.get(pk=a.pk)
+        self.assertEqual(len(a1.payment_set.all()), 3)
+        self.assertEqual(a1.date_from, None)
+        self.assertEqual(a1.date_to, None)
+
+        p1 = Payment.objects.get(account='23')
+        self.assertEqual(p1.date, datetime.date(day=1, month=2, year=2018))
+        self.assertEqual(p1.amount, 102)
+        self.assertEqual(p1.account, '23')
+        self.assertEqual(p1.bank_code, '23')
+        self.assertEqual(p1.VS, '120127010')
+        self.assertEqual(p1.VS2, None)
+        self.assertEqual(p1.SS, "")
+        self.assertEqual(p1.KS, '23322')
+        self.assertEqual(p1.user_identification, '')
+        self.assertEqual(p1.type, 'bank-transfer')
+        self.assertEqual(p1.done_by, "")
+        self.assertEqual(p1.account_name, "Mr. Test")
+        self.assertEqual(p1.bank_name, "")
+        self.assertEqual(p1.transfer_note, "note")
+        self.assertEqual(p1.currency, 'CZK')
+        self.assertEqual(p1.recipient_message, 'message')
+        self.assertEqual(p1.operation_id, '1')
+        self.assertEqual(p1.transfer_type, None)
+        self.assertEqual(p1.specification, None)
+        self.assertEqual(p1.order_id, None)
+        self.assertEqual(p1.user_donor_payment_channel, donor_channel)
+        self.assertEqual(p1.recipient_account, recipient_account)
+
+        self.assertEqual(donor_channel.payment_set.get(date=datetime.date(2018, 2, 1)), a1.payment_set.get(account='23'))
+
     def check_account_statement_data(self):
         self.run_commit_hooks()
 
