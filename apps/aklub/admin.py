auto-mat/klub

commit d9bda4cefc153a4833d7f463dc31da6eb0d79e49

translations, verbose names fixing

apps/aklub/admin.py
@@ -122,7 +122,7 @@ class PaymentsInline(nested_admin.NestedTabularInline):
 
 class DonorPaymentChannelInlineForm(forms.ModelForm):
     user_bank_account_char = forms.CharField(
-        label='user_bank_account',
+        label=_('user bank account'),
         max_length=50,
         required=False,
         )
@@ -195,33 +195,35 @@ class DonorPaymentChannelInline(admin.StackedInline):
         url = reverse('admin:aklub_payment_changelist')
         if obj.pk:
             redirect_button = mark_safe(
-                f"<a href='{url}?user_donor_payment_channel={obj.pk}'><input type='button' value='All payments'></a>"
+                f"<a href='{url}?user_donor_payment_channel={obj.pk}'><input type='button' value='{_('All payments')}'></a>"
             )
         else:
-            redirect_button = None
+            redirect_button = ''
         return redirect_button
     get_payment_list_link.short_description = _('All payments')
 
     def get_sum_amount(self, obj):
-        return obj.sum_amount
+        return obj.sum_amount if obj.sum_amount else ""
     get_sum_amount.short_description = _('Total amount')
 
     def get_payment_count(self, obj):
-        return obj.payment_count
+        return obj.payment_count if obj.payment_count else ""
     get_payment_count.short_description = _('Total payment count')
 
     def get_last_payment_date(self, obj):
-        return obj.last_payment_date
+        if not obj.id:
+            return ""
+        return obj.last_payment_date if obj.last_payment_date else ""
     get_last_payment_date.short_description = _('Last payment date')
 
     def get_dpch_details(self, obj):
         url = reverse('admin:aklub_donorpaymentchannel_change', args=(obj.pk,))
         if obj.pk:
             redirect_button = mark_safe(
-                                f"<a href='{url}'><input type='button' value='details'></a>"
-                                )
+                f"<a href='{url}'><input type='button' value={_('Details')}></a>"
+            )
         else:
-            redirect_button = None
+            redirect_button = ""
         return redirect_button
     get_dpch_details.short_description = _('DPCH details')
 
@@ -700,18 +702,6 @@ class MoneyAccountParentAdmin(PolymorphicParentModelAdmin):
         return {}
 
 
-class UserBankAccountAdmin(admin.ModelAdmin):
-    model = UserBankAccount
-
-    search_fields = (
-        'bank_account', 'bank_account_number',
-    )
-
-    list_filter = (
-        'bank_account', 'bank_account_number',
-    )
-
-
 class ProfileAdminMixin:
     """ ProfileAdmin mixin """
 
@@ -790,26 +780,26 @@ class ProfileAdminMixin:
         return ',\n'.join(results)
 
     total_payment.short_description = _("Total payment")
-    total_payment.admin_order_field = 'Total payment'
+    total_payment.admin_order_field = ("Total payment")
 
     def get_sum_amount(self, obj):
         return obj.sum_amount
-    get_sum_amount.admin_order_field = 'sum_amount'
+    get_sum_amount.admin_order_field = _("Sum of all payments")
     get_sum_amount.short_description = _("Sum of all payments")
 
     def get_payment_count(self, obj):
         return obj.payment_count
-    get_payment_count.admin_order_field = 'payment_count'
+    get_payment_count.admin_order_field = _("Payments count")
     get_payment_count.short_description = _("Payments count")
 
     def get_last_payment_date(self, obj):
         return obj.last_payment_date
-    get_last_payment_date.admin_order_field = 'last_payment_date'
+    get_last_payment_date.admin_order_field = _("Date of last payment")
     get_last_payment_date.short_description = _("Date of last payment")
 
     def get_first_payment_date(self, obj):
         return obj.first_payment_date
-    get_first_payment_date.admin_order_field = 'first_payment_date'
+    get_first_payment_date.admin_order_field = _('Date of first payment')
     get_first_payment_date.short_description = _("Date of first payment")
 
     def get_event(self, obj):
@@ -979,9 +969,9 @@ class DonorPaymentChannelLoaderClass(BaseInstanceLoader):
                                             money_account=money_account,
             )
         except Event.DoesNotExist:
-            raise ValidationError({'event': 'Event with this name doesnt exist'})
+            raise ValidationError({'event': _('Event with this name doesnt exist')})
         except MoneyAccount.DoesNotExist:
-            raise ValidationError({'moneyaccount': 'MoneyAccount with this bank_number doesnt exist'})
+            raise ValidationError({'moneyaccount': _('MoneyAccount with this bank_number doesnt exist')})
 
         except DonorPaymentChannel.DoesNotExist:
             return None
@@ -1017,7 +1007,7 @@ class DonorPaymentChannelResource(ModelResource):
 
     def before_import_row(self, row, **kwargs):
         if not row.get('profile_type') or row.get('profile_type') not in ['u', 'c']:
-            raise ValidationError({'profile_type': 'Insert "c" or "u" (company/user)'})
+            raise ValidationError({'profile_type': _('Insert "c" or "u" (company/user)')})
         row['email'] = row['email'].lower()
         try:
             if row.get('profile_type') == 'u':
@@ -1025,7 +1015,7 @@ class DonorPaymentChannelResource(ModelResource):
             else:
                 row['user'] = CompanyContact.objects.get(email=row['email']).company.id
         except (ProfileEmail.DoesNotExist, CompanyContact.DoesNotExist):
-            raise ValidationError({"email": "Company/User with this email doesn't exist"})
+            raise ValidationError({"email": _("Company/User with this email doesn't exist")})
 
     def import_obj(self, obj, data, dry_run):
         super(ModelResource, self).import_obj(obj, data, dry_run)
@@ -1199,6 +1189,8 @@ class DonorPaymetChannelAdmin(
         else:
             return obj.email_address_company
 
+    get_email.short_description = _("Main email")
+
     def get_name(self, obj):
         if obj.profile_type == UserProfile._meta.model_name:
             if obj.first_name or obj.last_name:
@@ -1207,6 +1199,7 @@ class DonorPaymetChannelAdmin(
                 return '-'
         else:
             return obj.company_name or '-'
+    get_name.short_description = _("Company/User name")
 
 
 def add_user_bank_acc_to_dpch(self, request, queryset):
@@ -1610,20 +1603,18 @@ class AccountStatementsAdmin(unit_admin_mixin_generator('administrative_unit'), 
     )
 
     def payments_count(self, obj):
+        # TODO: must be in annotation
         return obj.payment_set.count()
 
+    payments_count.admin_order_field = _("Total payments")
+    payments_count.short_description = _("Total payments")
+
     def paired_payments(self, obj):
+        # TODO: must be in annotation
         return obj.payment_set.filter(user_donor_payment_channel__isnull=False).count()
 
-    # TODO: add reporting of skipped payments to Celery task
-    # def save_model(self, request, obj, form, change):
-    #     if getattr(obj, 'skipped_payments', None):
-    #         skipped_payments_string = ', '.join(["%s %s (%s)" % (p['name'], p['surname'], p['email']) for p in obj.skipped_payments])
-    #         messages.info(request, 'Skipped payments: %s' % skipped_payments_string)
-    #     payments_without_user = ', '.join(["%s (%s)" % (p.account_name, p.user_identification) for p in obj.payments if not p.user])
-    #     if payments_without_user:
-    #         messages.info(request, 'Payments without user: %s' % payments_without_user)
-    #     obj.save()
+    paired_payments.admin_order_field = _("Paired payments")
+    paired_payments.short_description = _("Paired payments")
 
 
 def download_darujme_statement(self, request, queryset):
@@ -2047,7 +2038,7 @@ class UserProfileAdmin(
                 user = email.user
                 user.administrative_units.add(unit)
                 user.save()
-                messages.warning(request, 'User With this email is in database already. You can edit him now')
+                messages.warning(request, _('User With this email is in database already. You can edit him now'))
                 url = reverse('admin:aklub_userprofile_change', args=(user.pk,))
                 return HttpResponseRedirect(url)
 
@@ -2358,7 +2349,7 @@ class CompanyProfileAdmin(
                 unit = AdministrativeUnit.objects.get(id=data.get('administrative_units'))
                 company.administrative_units.add(unit)
                 company.save()
-                messages.warning(request, 'Company is in database already. You are able to make changes now.')
+                messages.warning(request, _('Company is in database already. You are able to make changes now.'))
                 url = reverse('admin:aklub_companyprofile_change', args=(company.pk,))
                 return HttpResponseRedirect(url)
 
@@ -2420,6 +2411,5 @@ admin.site.register(Recruiter, RecruiterAdmin)
 admin.site.register(TaxConfirmation, TaxConfirmationAdmin)
 admin.site.register(Source, SourceAdmin)
 admin.site.register(Profile, ProfileAdmin)
-admin.site.register(UserBankAccount, UserBankAccountAdmin)
 # register all adminactions
 actions.add_to_site(site)

apps/interactions/admin.py
@@ -3,6 +3,7 @@ from aklub.models import AdministrativeUnit, CompanyContact, Event, Profile, Pro
 from django.contrib import admin
 from django.core import serializers
 from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext_lazy as _
 
 from import_export import fields
 from import_export.admin import ImportExportMixin
@@ -57,9 +58,9 @@ class InteractionResource(ModelResource):
     def before_import_row(self, row, **kwargs): # noqa
         user = None
         if row.get('profile_type') not in ['u', 'c']:
-            raise ValidationError({'profile_type': 'Insert "c" or "u" (company/user)'})
+            raise ValidationError({'profile_type': _('Insert "c" or "u" (company/user)')})
         if not row.get('email') and not row.get('user'):
-            raise ValidationError({'email': 'Email or Username must be set'})
+            raise ValidationError({'email': _('Email or Username must be set')})
         if row.get('email'):
             row['email'] = row['email'].lower()
             try:
@@ -78,7 +79,7 @@ class InteractionResource(ModelResource):
             row['user'] = user.id
             return row
         else:
-            raise ValidationError({'user': 'User with this username or email doesnt exist'})
+            raise ValidationError({'user': _('User with this username or email doesnt exist')})
 
     def dehydrate_user(self, interaction):
         if hasattr(interaction, 'user'):

apps/aklub/__init__.py
@@ -1,0 +1,1 @@
+default_app_config = 'apps.apps.InteractionConfig'

apps/aklub/migrations/0088_auto_20201204_1045.py
@@ -1,27 +1,0 @@
-# Generated by Django 2.2.16 on 2020-12-04 09:45
-
-from django.db import migrations, models
-
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('aklub', '0087_auto_20200918_1159'),
-    ]
-
-    def delete_unused_emails(apps, schema_editor):
-        """
-        Delete unused profileemails (which has duplicity emails)
-        """
-        ProfileEmail = apps.get_model('aklub', 'ProfileEmail')
-        ProfileEmail.objects.filter(user__isnull=True).delete()
-
-    operations = [
-        migrations.RunPython(delete_unused_emails, reverse_code=migrations.RunPython.noop),
-        migrations.AlterField(
-            model_name='profileemail',
-            name='email',
-            field=models.EmailField(default='', max_length=254, unique=True, verbose_name='email address'),
-            preserve_default=False,
-        ),
-    ]

apps/aklub/migrations/0088_auto_20201204_1729.py
@@ -1,0 +1,117 @@
+# Generated by Django 2.2.16 on 2020-12-04 16:29
+
+from django.db import migrations, models
+import django.db.models.deletion
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aklub', '0087_auto_20200918_1159'),
+    ]
+    def delete_unused_emails(apps, schema_editor):
+        """
+        Delete unused profileemails (which has duplicity emails)
+        """
+        ProfileEmail = apps.get_model('aklub', 'ProfileEmail')
+        ProfileEmail.objects.filter(user__isnull=True).delete()
+
+    operations = [
+        migrations.RunPython(delete_unused_emails, reverse_code=migrations.RunPython.noop),
+        migrations.AlterModelOptions(
+            name='administrativeunit',
+            options={'verbose_name': 'Administrative unit', 'verbose_name_plural': 'Administrative units'},
+        ),
+        migrations.AlterModelOptions(
+            name='apiaccount',
+            options={'verbose_name': 'Api Account', 'verbose_name_plural': 'Api Accounts'},
+        ),
+        migrations.AlterModelOptions(
+            name='companycontact',
+            options={'verbose_name': 'Company contact', 'verbose_name_plural': 'Company contacts'},
+        ),
+        migrations.AlterModelOptions(
+            name='moneyaccount',
+            options={'verbose_name': 'Bank/Api account', 'verbose_name_plural': 'Bank/Api accounts'},
+        ),
+        migrations.AlterModelOptions(
+            name='preference',
+            options={'verbose_name': 'Preference', 'verbose_name_plural': 'Preferences'},
+        ),
+        migrations.AlterField(
+            model_name='accountstatements',
+            name='csv_file',
+            field=models.FileField(upload_to='account-statements', verbose_name='csv file'),
+        ),
+        migrations.AlterField(
+            model_name='accountstatements',
+            name='date_from',
+            field=models.DateField(blank=True, null=True, verbose_name='Date from'),
+        ),
+        migrations.AlterField(
+            model_name='accountstatements',
+            name='date_to',
+            field=models.DateField(blank=True, null=True, verbose_name='Date to'),
+        ),
+        migrations.AlterField(
+            model_name='accountstatements',
+            name='import_date',
+            field=models.DateTimeField(auto_now=True, verbose_name='Import date'),
+        ),
+        migrations.AlterField(
+            model_name='accountstatements',
+            name='type',
+            field=models.CharField(choices=[('account', 'Account statement - Fio Banka'), ('account_cs', 'Account statement - Česká spořitelna'), ('account_kb', 'Account statement - Komerční Banka'), ('account_csob', 'Account statement - ČSOB'), ('account_sberbank', 'Account statement - Sberbank'), ('account_raiffeisenbank', 'Account statement - Raiffeisenbank '), ('darujme', 'Darujme.cz')], max_length=30, verbose_name='Type'),
+        ),
+        migrations.AlterField(
+            model_name='automaticcommunication',
+            name='method_type',
+            field=models.ForeignKey(help_text='Interaction type with allowed sending', limit_choices_to=models.Q(('send_sms', True), ('send_email', True), _connector='OR'), null=True, on_delete=django.db.models.deletion.CASCADE, to='interactions.InteractionType', verbose_name='Method/Type of Interaction'),
+        ),
+        migrations.AlterField(
+            model_name='bankaccount',
+            name='bank_account',
+            field=models.CharField(blank=True, max_length=50, null=True, verbose_name='Bank Account'),
+        ),
+        migrations.AlterField(
+            model_name='donorpaymentchannel',
+            name='money_account',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='moneyaccounts', to='aklub.MoneyAccount', verbose_name='Bank/Api account'),
+        ),
+        migrations.AlterField(
+            model_name='donorpaymentchannel',
+            name='regular_frequency',
+            field=models.CharField(blank=True, choices=[(None, '---'), ('monthly', 'Monthly'), ('quaterly', 'Quaterly'), ('biannually', 'Bianually'), ('annually', 'Anually')], max_length=20, null=True, verbose_name='Frequency of regular payments'),
+        ),
+        migrations.AlterField(
+            model_name='donorpaymentchannel',
+            name='user_bank_account',
+            field=models.ForeignKey(blank=True, default=None, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='userbankaccounts', to='aklub.UserBankAccount', verbose_name='User bank account'),
+        ),
+        migrations.AlterField(
+            model_name='masscommunication',
+            name='method_type',
+            field=models.ForeignKey(help_text='Interaction type with allowed sending', limit_choices_to=models.Q(('send_sms', True), ('send_email', True), _connector='OR'), null=True, on_delete=django.db.models.deletion.CASCADE, to='interactions.InteractionType', verbose_name='Method/Type of Interaction'),
+        ),
+        migrations.AlterField(
+            model_name='profileemail',
+            name='email',
+            field=models.EmailField(default='', max_length=254, unique=True, verbose_name='email address'),
+            preserve_default=False,
+        ),
+        migrations.AlterField(
+            model_name='taxconfirmation',
+            name='amount',
+            field=models.PositiveIntegerField(default=0, verbose_name='Amount'),
+        ),
+        migrations.AlterField(
+            model_name='taxconfirmation',
+            name='pdf_type',
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='smmapdfs.PdfSandwichType', verbose_name='PDF type'),
+        ),
+        migrations.AlterField(
+            model_name='taxconfirmation',
+            name='year',
+            field=models.PositiveIntegerField(verbose_name='Year'),
+        ),
+    ]

apps/aklub/models.py
@@ -182,6 +182,10 @@ ICO_ERROR_MESSAGE = _("IČO není zadáno ve správném formátu. Zkontrolujte �
 
 
 class AdministrativeUnit(models.Model, ParseAccountStatement):
+    class Meta:
+        verbose_name = _("Administrative unit")
+        verbose_name_plural = _("Administrative units")
+
     name = models.CharField(
         verbose_name=_("Name"),
         max_length=255,
@@ -1034,8 +1038,9 @@ class UserProfile(Profile, AbstractUserProfile):
 
 
 class CompanyContact(models.Model):
-
     class Meta:
+        verbose_name = _("Company contact")
+        verbose_name_plural = _("Company contacts")
         unique_together = (
             ('is_primary', 'administrative_unit', 'email'),
             ('is_primary', 'administrative_unit', 'company'),
@@ -1198,6 +1203,9 @@ def Userprofile_administrative_unit_changed(sender, **kwargs):
 
 
 class Preference(models.Model):
+    class Meta:
+        verbose_name = _("Preference")
+        verbose_name_plural = _("Preferences")
     user = models.ForeignKey(
         Profile,
         blank=True,
@@ -1296,10 +1304,9 @@ class Telephone(models.Model):
         super().validate_unique(exclude=exclude)
 
     def check_duplicate(self, *args, **kwargs):
-        qs = Telephone.objects.filter(telephone=self.telephone, user=self.user)
         if self.pk is None:
-            if qs.filter(telephone=self.telephone, user=self.user).exists():
-                raise ValidationError("Duplicate phone number for this user")
+            if Telephone.objects.filter(telephone=self.telephone, user=self.user).exists():
+                raise ValidationError(_("Duplicate phone number for this user"))
 
     def clean(self, *args, **kwargs):
         self.check_duplicate()
@@ -1660,18 +1667,21 @@ class AccountStatements(ParseAccountStatement, models.Model):
         ('darujme', 'Darujme.cz'),
     )
 
-    type = models.CharField(max_length=30, choices=TYPE_OF_STATEMENT)  # noqa
-    import_date = models.DateTimeField(auto_now=True)
+    type = models.CharField(max_length=30, choices=TYPE_OF_STATEMENT, verbose_name=_("Type"))  # noqa
+    import_date = models.DateTimeField(auto_now=True, verbose_name=_("Import date"),)
     csv_file = models.FileField(
+        verbose_name=_("csv file"),
         upload_to='account-statements',
         null=False,
         blank=False,
     )
     date_from = models.DateField(
+        verbose_name=_("Date from"),
         blank=True,
         null=True,
     )
     date_to = models.DateField(
+        verbose_name=_("Date to"),
         blank=True,
         null=True,
     )
@@ -1737,6 +1747,10 @@ class AccountStatements(ParseAccountStatement, models.Model):
 
 
 class MoneyAccount(PolymorphicModel):
+    class Meta:
+        verbose_name = _("Bank/Api account")
+        verbose_name_plural = _("Bank/Api accounts")
+
     note = models.TextField(
         verbose_name=_("Bank account note"),
         blank=True,
@@ -1766,6 +1780,7 @@ class BankAccount(MoneyAccount):
         verbose_name_plural = _("Bank accounts")
 
     bank_account = models.CharField(
+        verbose_name=_("Bank Account"),
         max_length=50,
         blank=True,
         null=True,
@@ -1782,6 +1797,10 @@ class BankAccount(MoneyAccount):
 
 
 class ApiAccount(MoneyAccount):
+    class Meta:
+        verbose_name = _("Api Account")
+        verbose_name_plural = _("Api Accounts")
+
     project_name = models.CharField(
         verbose_name=_("Name"),
         unique=True,
@@ -1944,11 +1963,13 @@ class DonorPaymentChannel(ComputedFieldsModel):
     )
     money_account = models.ForeignKey(
         MoneyAccount,
+        verbose_name=_("Bank/Api account"),
         related_name='moneyaccounts',
         on_delete=models.CASCADE,
     )
     user_bank_account = models.ForeignKey(
         UserBankAccount,
+        verbose_name=_("User bank account"),
         related_name='userbankaccounts',
         on_delete=models.CASCADE,
         default=None,
@@ -1994,7 +2015,7 @@ class DonorPaymentChannel(ComputedFieldsModel):
                 )
             if qs:
                 if qs.first().pk != self.pk:
-                    raise ValidationError("Duplicate VS")
+                    raise ValidationError(_("Duplicate VS"))
         except MoneyAccount.DoesNotExist:
             pass
 
@@ -2004,7 +2025,7 @@ class DonorPaymentChannel(ComputedFieldsModel):
         """
         return self.payment_set.aggregate(count=Count('amount'))['count']
 
-    number_of_payments.short_description = _("# payments")
+    number_of_payments.short_description = _("Number of payments")
     number_of_payments.admin_order_field = 'payments_number'
 
     def last_payment_function(self):
@@ -2109,6 +2130,8 @@ class DonorPaymentChannel(ComputedFieldsModel):
     @computed(models.FloatField(null=True), depends=['payment_set'])
     def payment_total(self):
         return self.payment_set.aggregate(sum=Sum('amount'))['sum'] or 0
+
+    payment_total.short_description = _("Payment total")
 
     def total_contrib_string(self):
         """Return the sum of all money received from this user
@@ -2546,6 +2569,7 @@ class AutomaticCommunication(models.Model):
     )
     method_type = models.ForeignKey(
         "interactions.interactiontype",
+        verbose_name=_('Method/Type of Interaction'),
         help_text=_("Interaction type with allowed sending"),
         on_delete=models.CASCADE,
         limit_choices_to=Q(send_sms=True) | Q(send_email=True),
@@ -2658,6 +2682,7 @@ class MassCommunication(models.Model):
     )
     method_type = models.ForeignKey(
         "interactions.interactiontype",
+        verbose_name=_('Method/Type of Interaction'),
         on_delete=models.CASCADE,
         help_text=_("Interaction type with allowed sending"),
         limit_choices_to=Q(send_sms=True) | Q(send_email=True),
@@ -2755,7 +2780,7 @@ class MassCommunication(models.Model):
     def clean(self):
         if self.attach_tax_confirmation:
             if not self.attached_tax_confirmation_year or not self.attached_tax_confirmation_type:
-                raise ValidationError("YEAR and PDF_TYPE must be set")
+                raise ValidationError(_("YEAR and PDF_TYPE must be set"))
         super().clean()
 
 
@@ -2826,11 +2851,12 @@ class TaxConfirmation(models.Model):
         null=False,
         blank=False,
     )
-    year = models.PositiveIntegerField()
-    amount = models.PositiveIntegerField(default=0)
+    year = models.PositiveIntegerField(verbose_name=_('Year'))
+    amount = models.PositiveIntegerField(default=0, verbose_name=_('Amount'))
     file = models.FileField(storage=OverwriteStorage())  # DEPRICATED!
     pdf_type = models.ForeignKey(
                     'smmapdfs.PdfSandwichType',
+                    verbose_name=_('PDF type'),
                     on_delete=models.SET_NULL,
                     null=True,
                     blank=True,

apps/aklub/profile_model_resources_mixin.py
@@ -3,6 +3,7 @@ import datetime
 
 from django.core.exceptions import ValidationError
 from django.utils.timezone import make_aware
+from django.utils.translation import ugettext_lazy as _
 
 from import_export import fields
 from import_export.resources import ModelResource
@@ -229,7 +230,7 @@ def before_import_row(self, row, **kwargs):
     if row.get('username') == "":
         row["username"] = None
     if not row['administrative_units']:
-        raise ValidationError({'administrative_units': 'This field must be set'})
+        raise ValidationError({'administrative_units': _('This field must be set')})
 
 
 def import_field(self, field, obj, data, is_m2m=False):

commit dd086b59d80e1f3d2887e03443e6dfc10018e547

Fix ordering bug introduced by recent commit
https://github.com/auto-mat/klub/commit/d9bda4cefc153a4833d7f463dc31da6eb0d79e49

apps/aklub/admin.py
@@ -780,26 +780,25 @@ class ProfileAdminMixin:
         return ',\n'.join(results)
 
     total_payment.short_description = _("Total payment")
-    total_payment.admin_order_field = ("Total payment")
 
     def get_sum_amount(self, obj):
         return obj.sum_amount
-    get_sum_amount.admin_order_field = _("Sum of all payments")
+    get_sum_amount.admin_order_field = 'sum_amount'
     get_sum_amount.short_description = _("Sum of all payments")
 
     def get_payment_count(self, obj):
         return obj.payment_count
-    get_payment_count.admin_order_field = _("Payments count")
+    get_payment_count.admin_order_field = 'payment_count'
     get_payment_count.short_description = _("Payments count")
 
     def get_last_payment_date(self, obj):
         return obj.last_payment_date
-    get_last_payment_date.admin_order_field = _("Date of last payment")
+    get_last_payment_date.admin_order_field = 'last_payment_date'
     get_last_payment_date.short_description = _("Date of last payment")
 
     def get_first_payment_date(self, obj):
         return obj.first_payment_date
-    get_first_payment_date.admin_order_field = _('Date of first payment')
+    get_first_payment_date.admin_order_field = 'first_payment_date'
     get_first_payment_date.short_description = _("Date of first payment")
 
     def get_event(self, obj):
@@ -1606,14 +1605,12 @@ class AccountStatementsAdmin(unit_admin_mixin_generator('administrative_unit'), 
         # TODO: must be in annotation
         return obj.payment_set.count()
 
-    payments_count.admin_order_field = _("Total payments")
     payments_count.short_description = _("Total payments")
 
     def paired_payments(self, obj):
         # TODO: must be in annotation
         return obj.payment_set.filter(user_donor_payment_channel__isnull=False).count()
 
-    paired_payments.admin_order_field = _("Paired payments")
     paired_payments.short_description = _("Paired payments")
 
 