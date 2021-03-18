308ea5a9856f578031c035e5be6199f950cc257a
Petr Kůdela <petrkudela94@seznam.cz>
2021-03-18 16:26:28 +0100

    get all admins, not users

--- apps/notifications_edit/utils.py
+++ apps/notifications_edit/utils.py
@@ -7,7 +7,7 @@ def send_notification_to_is_staff_members(administrative_unit, verb, description
     """
     sending notification to every user of admin team uder administrative_unit
     """
-    profiles = Profile.objects.filter(is_staff=True, administrative_units=administrative_unit)
+    profiles = Profile.objects.filter(is_staff=True, administrated_units=administrative_unit)
     notify.send(
         sender=administrative_unit,
         recipient=profiles,
