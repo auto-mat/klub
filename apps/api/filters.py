da1f752f4b5aa148d310db9d8e77a36e6fae9bfa
Petr Kůdela <petrkudela94@seznam.cz>
2021-04-30 10:59:42 +0200

    add some filters

--- apps/api/filters.py
+++ apps/api/filters.py
@@ -5,12 +5,26 @@ from events.models import Event
 
 class EventCustomFilter(filters.FilterSet):
     administrative_unit = filters.CharFilter(method='get_administrative_unit')
+    event_type_array = filters.CharFilter(method='get_event_type_array')
 
     class Meta:
         model = Event
-        fields = ["slug"]
+        fields = {
+            'date_from': ['gte', 'lte', 'year'],
+            'date_to': ['gte', 'lte', 'year'],
+            'start_date': ['gte', 'lte', 'year'],
+            'indended_for': ['exact'],
+            'program': ['exact'],
+            'is_internal': ['exact'],
+        }
 
     def get_administrative_unit(self, queryset, name, value, *args, **kwargs):
         if name == 'administrative_unit':
             queryset = queryset.filter(administrative_units__slug=value)
+        return queryset
+
+    def get_event_type_array(self, queryset, name, value, *args, **kwargs):
+        if name == 'event_type_array':
+            event_type_list = value.split(",")  # slugs
+            queryset = queryset.filter(event_type__slug__in=event_type_list)
         return queryset
