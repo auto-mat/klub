f3959f043f64cdfabaf1dffe7adfdf7793da210e
Timothy Hobbs <timothy@hobbs.cz>
2021-12-04 15:22:53 +0100

    Fix advanced_filters migration fix thingamagig

--- apps/aklub/migrations/data_migration/fix_advanced_filters_app_table_column.py
+++ apps/aklub/migrations/data_migration/fix_advanced_filters_app_table_column.py
@@ -9,20 +9,29 @@ COLUMN_NAME = 'userprofile_id'
 def forwards_func(apps, schema_editor):
     with schema_editor.connection.cursor() as cursor:
         cursor.execute(
-            ('ALTER TABLE advanced_filters_advancedfilter_users '
-             'RENAME {}_id TO {}').format(
-                 get_user_model()._meta.model_name,
-                 COLUMN_NAME
-             ),
+            f"""
+            SELECT EXISTS (SELECT 1
+            FROM information_schema.columns
+            WHERE table_name='advanced_filters_advancedfilter_users' AND column_name='{COLUMN_NAME}');
+            """
         )
+        column = cursor.fetchall()
+        if column[0][0]:
+            cursor.execute(
+                ('ALTER TABLE advanced_filters_advancedfilter_users '
+                 'RENAME {} TO {}_id').format(
+                     COLUMN_NAME,
+                     get_user_model()._meta.model_name,
+                 ),
+            )
 
 
 def reverse_func(apps, schema_editor):
     with schema_editor.connection.cursor() as cursor:
         cursor.execute(
             ('ALTER TABLE advanced_filters_advancedfilter_users '
-             'RENAME {} TO {}_id').format(
-                 COLUMN_NAME,
+             'RENAME {}_id TO {}').format(
                  get_user_model()._meta.model_name,
+                 COLUMN_NAME,
              ),
         )
