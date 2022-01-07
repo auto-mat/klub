364b6f941d1eba3ec79c1fda08f539da2e738391
Timothy Hobbs <timothy@hobbs.cz>
2022-01-07 11:50:57 +0100

    Don't print coverage report to terminal

--- single-pytest.sh
+++ single-pytest.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-pytest $1 --cov
+pytest $1 --cov-report= --cov
 coveragepy-lcov --data_file_path .coverage --output_file_path lcov.info
 sed -i 's/\/klub-v/\/home\/timothy\/pu\/auto-mat\/klub/g' lcov.info
