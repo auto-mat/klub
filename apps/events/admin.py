8422a85174289fa8fd38e3aba10131f9f769b95c
Tomas Zigo <tomas.zigo@slovanet.sk>
2023-03-27 19:40:44 +0200

    apps/events: register Location admin model specific_name list display field

--- apps/events/admin.py
+++ apps/events/admin.py
@@ -54,7 +54,14 @@ class OrganizationPositionAdmin(admin.ModelAdmin):
 
 @admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
-    list_display = ("name", "place", "region", "gps_latitude", "gps_longitude")
+    list_display = (
+        "name",
+        "specific_name",
+        "place",
+        "region",
+        "gps_latitude",
+        "gps_longitude",
+    )
 
 
 @admin.register(EventType)
