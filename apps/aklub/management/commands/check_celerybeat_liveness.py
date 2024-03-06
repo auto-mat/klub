857c7de062c745d2c6ef99ed6848caaf1514ff3a
Tomas Zigo <tomas.zigo@slovanet.sk>
2024-03-06 12:40:16 +0100

    apps/aklub/management/commands: remove unused modules import

--- apps/aklub/management/commands/check_celerybeat_liveness.py
+++ apps/aklub/management/commands/check_celerybeat_liveness.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-from urllib.parse import urlparse
 
-import redis
-
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 from aklub.tasks import check_celerybeat_liveness
